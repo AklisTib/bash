fd3199a73c09f0765d0e348578e4676a3ca511a6
Alex <147999158+AklisTib@users.noreply.github.com>
2024-04-04 12:07:49 +0300

    Update zabbixcli_alt.sh

--- zabbixcli_alt.sh
+++ zabbixcli_alt.sh
@@ -4,8 +4,8 @@ echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 apt-get update && apt-get install zabbix-agent -y
 
-sed -i 's/Server=127.0.0.1/Server=10.0.2.15/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/Server=10.0.2.15/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Server=127.0.0.1/Server=192. 168.1.108/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/ServerActive=127.0.0.1/Server=192.168.1.108/g' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/Hostname=localhost.localdomain/Hostname=Client1/g' /etc/zabbix/zabbix_agentd.conf
 
 systemctl enable --now zabbix_agentd.service
